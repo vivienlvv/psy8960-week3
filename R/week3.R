bf192272e99c1b68150e290fd1d4a8ddcd0c3bb0
Vivien Lee <vivienlvv@gmail.com>
2023-02-03 16:12:55 -0600

    Commit for Part 2

--- R/week3.R
+++ R/week3.R
@@ -1,2 +1,9 @@
 # Script Settings and Resources
 setwd(dirname(rstudioapi::getActiveDocumentContext()$path))
+
+# Data Import and Cleaning
+raw_df = read.csv("../data/week3.csv")
+raw_df$timeStart = as.POSIXct(raw_df$timeStart, format = "%Y-%m-%d %H:%M:%S")
+raw_df$timeEnd = as.POSIXct(raw_df$timeEnd, format = "%Y-%m-%d %H:%M:%S")
+clean_df = raw_df[!grepl(pattern = "2017-06", raw_df$timeStart),]
+clean_df = clean_df[clean_df$q6 == 1,]
