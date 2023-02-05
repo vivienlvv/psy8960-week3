1f043384359b5f2ffac2cb01e589616fae621aea
Vivien Lee <vivienlvv@gmail.com>
2023-02-04 21:50:10 -0600

    Commit for Part 3

--- R/week3.R
+++ R/week3.R
@@ -7,3 +7,12 @@ raw_df$timeStart = as.POSIXct(raw_df$timeStart, format = "%Y-%m-%d %H:%M:%S")
 raw_df$timeEnd = as.POSIXct(raw_df$timeEnd, format = "%Y-%m-%d %H:%M:%S")
 clean_df = raw_df[!grepl(pattern = "2017-06", raw_df$timeStart),]
 clean_df = clean_df[clean_df$q6 == 1,]
+
+# Analysis 
+clean_df$timeSpent = difftime(time1 = clean_df$timeEnd, time2 = clean_df$timeStart)
+hist(as.numeric(clean_df$timeSpent))
+frequency_tables_list = lapply(clean_df[,5:14], table)
+lapply(frequency_tables_list, barplot)
+sum(clean_df$q1 >= clean_df$q2 & clean_df$q2 != clean_df$q3)
+for(i in frequency_tables_list){
+  barplot(i)}
