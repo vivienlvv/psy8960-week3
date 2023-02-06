fc0b5c445f57b7537508c31d9d8ababacfe02580
Vivien Lee <vivienlvv@gmail.com>
2023-02-05 20:08:27 -0600

    Changing difftime() to reflect time spent in s

--- R/week3.R
+++ R/week3.R
@@ -9,7 +9,7 @@ clean_df = raw_df[!grepl(pattern = "2017-06", raw_df$timeStart),]
 clean_df = clean_df[clean_df$q6 == 1,]
 
 # Analysis 
-clean_df$timeSpent = difftime(time1 = clean_df$timeEnd, time2 = clean_df$timeStart, units = "mins")
+clean_df$timeSpent = difftime(time1 = clean_df$timeEnd, time2 = clean_df$timeStart, units = "s")
 hist(as.numeric(clean_df$timeSpent))
 frequency_tables_list = lapply(clean_df[,5:14], table)
 lapply(frequency_tables_list, barplot)
